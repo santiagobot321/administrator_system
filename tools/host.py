192eb985face22da8a131bff89a5261bc63f2957
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-08-24 20:04:49 -0500

    añadiendo host

---
+++ tools/host.py
@@ -0,0 +1,13 @@
+import socket
+
+def send_shutdown(ip, port=9876):
+    """Connect to the agent and send shutdown command"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    s.sendall(b"sudo shutdown now")
+    s.close()
+    print(f"Shutdown command sent to {ip}:{port}")
+
+if __name__ == "__main__":
+    vm_ip = "192.168.1.93"  # <-- cámbialo por la IP de tu VM
+    send_shutdown(vm_ip)
